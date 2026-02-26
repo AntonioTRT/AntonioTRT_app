dd50e2b7ff5a4f859ac291403f4d8c75b11eb637
Estrada <550016588@gehealthcare.com>
2026-02-26 16:49:49 +0200

    new ui

--- ui/window.py
+++ ui/window.py
@@ -1,7 +1,9 @@
 
-from PySide6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QLabel
+from PySide6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QLabel, QMenuBar, QMenu
+from PySide6.QtGui import QAction
 from PySide6.QtUiTools import QUiLoader
-from PySide6.QtCore import QFile
+from PySide6.QtCore import QFile, Qt
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -11,12 +13,111 @@ class MainWindow(QMainWindow):
         ui_file.open(QFile.ReadOnly)
         self.ui = loader.load(ui_file, self)
         ui_file.close()
-        self.ui.showFullScreen()
-        # Conectar el botón 'Inicio' a la función para mostrar la página de Antonio
+        self.setCentralWidget(self.ui)
+        self.setWindowTitle("Hola App")
+        self.showFullScreen()
+
+        # Tema oscuro
+        dark_stylesheet = """
+            QMainWindow, QWidget {
+                background-color: #111;
+                color: #fff;
+            }
+            QPushButton {
+                background-color: #222;
+                color: #fff;
+                border-radius: 8px;
+            }
+            QMenuBar, QMenu {
+                background-color: #222;
+                color: #fff;
+            }
+        """
+        self.setStyleSheet(dark_stylesheet)
+
+        # Menú clásico
+        menubar = QMenuBar(self)
+        file_menu = QMenu("File", self)
+        edit_menu = QMenu("Edit", self)
+        about_menu = QMenu("About", self)
+
+        file_menu.addAction(QAction("New", self))
+        file_menu.addAction(QAction("Open", self))
+        file_menu.addSeparator()
+        file_menu.addAction(QAction("Exit", self, triggered=self.close))
+
+        edit_menu.addAction(QAction("Undo", self))
+
+        about_menu.addAction(QAction("About", self))
+
+        menubar.addMenu(file_menu)
+        menubar.addMenu(edit_menu)
+        menubar.addMenu(about_menu)
+        self.setMenuBar(menubar)
+
+        # Conectar el botón 'Inicio' a la función para mostrar la segunda ventana
         start_button = self.ui.findChild(type(self.ui.findChild(QLabel)), "startButton")
         if start_button is not None:
-            start_button.clicked.connect(self.show_antonio_page)
+            start_button.clicked.connect(self.show_second_window)
+
+    def show_second_window(self):
+        second_window = SecondWindow()
+        second_window.showFullScreen()
+        second_window.show()
         self.close()
+
+
+class SecondWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Segunda Ventana")
+        self.setGeometry(100, 100, 800, 600)
+        # Tema oscuro
+        dark_stylesheet = """
+            QMainWindow, QWidget {
+                background-color: #111;
+                color: #fff;
+            }
+            QPushButton {
+                background-color: #222;
+                color: #fff;
+                border-radius: 8px;
+            }
+            QMenuBar, QMenu {
+                background-color: #222;
+                color: #fff;
+            }
+        """
+        self.setStyleSheet(dark_stylesheet)
+
+        # Menú clásico
+        menubar = QMenuBar(self)
+        file_menu = QMenu("File", self)
+        edit_menu = QMenu("Edit", self)
+        about_menu = QMenu("About", self)
+
+        file_menu.addAction(QAction("New", self))
+        file_menu.addAction(QAction("Open", self))
+        file_menu.addSeparator()
+        file_menu.addAction(QAction("Exit", self, triggered=self.close))
+
+        edit_menu.addAction(QAction("Undo", self))
+
+        about_menu.addAction(QAction("About", self))
+
+        menubar.addMenu(file_menu)
+        menubar.addMenu(edit_menu)
+        menubar.addMenu(about_menu)
+        self.setMenuBar(menubar)
+
+        # Contenido de la segunda ventana
+        widget = QWidget()
+        layout = QVBoxLayout(widget)
+        label = QLabel("¡Bienvenido a la segunda ventana!")
+        label.setAlignment(Qt.AlignCenter)
+        label.setStyleSheet("font-size: 48px;")
+        layout.addWidget(label)
+        self.setCentralWidget(widget)
 
     def show_antonio_page(self):
         antonio_widget = QWidget()
