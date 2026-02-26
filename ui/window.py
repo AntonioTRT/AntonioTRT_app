f05a006432f90cb83a008c82aded9aaa5fee44f9
Estrada <550016588@gehealthcare.com>
2026-02-26 19:30:49 +0200

    new ui

--- ui/window.py
+++ ui/window.py
@@ -5,34 +5,42 @@ from PySide6.QtUiTools import QUiLoader
 from PySide6.QtCore import QFile, Qt
 
 
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        # Cargar UI
         loader = QUiLoader()
         ui_file = QFile("ui/ventana.ui")
         ui_file.open(QFile.ReadOnly)
-        self.ui = loader.load(ui_file, self)
+        ui = loader.load(ui_file, self)
         ui_file.close()
-        self.setCentralWidget(self.ui)
-        self.setWindowTitle("Hola App")
-        self.showFullScreen()
+
+        # Asignar centralWidget
+        central_widget = ui.findChild(QWidget, "centralwidget")
+        # Buscar botón antes de asignar centralWidget
+        start_button = central_widget.findChild(QWidget, "startButton")
+        if start_button:
+            start_button.clicked.connect(self.show_second_window)
+        self.setCentralWidget(central_widget)
 
         # Tema oscuro
-        dark_stylesheet = """
-            QMainWindow, QWidget {
-                background-color: #111;
-                color: #fff;
-            }
-            QPushButton {
-                background-color: #222;
-                color: #fff;
-                border-radius: 8px;
-            }
-            QMenuBar, QMenu {
-                background-color: #222;
-                color: #fff;
-            }
-        """
+        dark_stylesheet = (
+            "QMainWindow, QWidget {"
+            "background-color: #111;"
+            "color: #fff;"
+            "}"
+            "QPushButton {"
+            "background-color: #222;"
+            "color: #fff;"
+            "border-radius: 8px;"
+            "}"
+            "QMenuBar, QMenu {"
+            "background-color: #222;"
+            "color: #fff;"
+            "}"
+        )
         self.setStyleSheet(dark_stylesheet)
 
         # Menú clásico
@@ -55,40 +63,74 @@ class MainWindow(QMainWindow):
         menubar.addMenu(about_menu)
         self.setMenuBar(menubar)
 
-        # Conectar el botón 'Inicio' a la función para mostrar la segunda ventana
-        start_button = self.ui.findChild(type(self.ui.findChild(QLabel)), "startButton")
-        if start_button is not None:
-            start_button.clicked.connect(self.show_second_window)
+        self.setWindowTitle("Hola App")
+        self.showFullScreen()
+
 
     def show_second_window(self):
         second_window = SecondWindow()
         second_window.showFullScreen()
         second_window.show()
         self.close()
+        from PySide6.QtWidgets import QApplication
+        QApplication.instance().quit()
 
 
 class SecondWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        widget = QWidget()
+        layout = QVBoxLayout(widget)
+        label = QLabel("¡Bienvenido a la segunda ventana!")
+        label.setAlignment(Qt.AlignCenter)
+        label.setStyleSheet("font-size: 48px;")
+        layout.addWidget(label)
+        self.setCentralWidget(widget)
         self.setWindowTitle("Segunda Ventana")
-        self.setGeometry(100, 100, 800, 600)
+        self.showFullScreen()
         # Tema oscuro
-        dark_stylesheet = """
-            QMainWindow, QWidget {
-                background-color: #111;
-                color: #fff;
-            }
-            QPushButton {
-                background-color: #222;
-                color: #fff;
-                border-radius: 8px;
-            }
-            QMenuBar, QMenu {
-                background-color: #222;
-                color: #fff;
-            }
-        """
+        dark_stylesheet = (
+            "QMainWindow, QWidget {"
+            "background-color: #111;"
+            "color: #fff;"
+            "}"
+            "QPushButton {"
+            "background-color: #222;"
+            "color: #fff;"
+            "border-radius: 8px;"
+            "}"
+            "QMenuBar, QMenu {"
+            "background-color: #222;"
+            "color: #fff;"
+            "}"
+        )
         self.setStyleSheet(dark_stylesheet)
+
+        # Menú clásico
+        menubar = QMenuBar(self)
+        file_menu = QMenu("File", self)
+        edit_menu = QMenu("Edit", self)
+        about_menu = QMenu("About", self)
+
+        file_menu.addAction(QAction("New", self))
+        file_menu.addAction(QAction("Open", self))
+        file_menu.addSeparator()
+        file_menu.addAction(QAction("Exit", self, triggered=self.close))
+
+        edit_menu.addAction(QAction("Undo", self))
+
+        about_menu.addAction(QAction("About", self))
+
+        menubar.addMenu(file_menu)
+        menubar.addMenu(edit_menu)
+        menubar.addMenu(about_menu)
+        self.setMenuBar(menubar)
+        self.setStyleSheet(dark_stylesheet)
+
+        # Conectar el botón 'inicio' desde el .ui
+        start_button = self.ui.findChild(QWidget, "startButton")
+        if start_button:
+            start_button.clicked.connect(self.show_second_window)
 
         # Menú clásico
         menubar = QMenuBar(self)
